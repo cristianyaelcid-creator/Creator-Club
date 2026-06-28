c9356053635a4bb4d3879a400d670d746cc266dc
cristianyaelcid-creator <cristianyaelcid@gmail.com>
2026-06-28 16:26:49 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+const API_URL = "https://script.google.com/macros/s/AKfycbxnNeaSjbg9xEj07OZbhwvWPOJYu38cCJFhdpKm_OdTaeVSSnTicp-qRCAScBXE3_zhXw/exec";
 
+fetch(API_URL)
+  .then(response => response.json())
+  .then(datos => {
+
+    // Tomamos el primer cliente (Jesús)
+    const cliente = datos[1];
+
+    document.getElementById("nombre").textContent = cliente[1];
+
+    document.getElementById("puntos").textContent =
+      cliente[2] + " punto(s)";
+
+    document.getElementById("compras").textContent =
+      cliente[3] + " de 10 compras";
+
+    const faltan = 10 - cliente[3];
+
+    document.getElementById("recompensa").textContent =
+      "🎁 Te faltan " + faltan + " compras";
+
+    document.getElementById("barra").style.width =
+      (cliente[3] * 10) + "%";
+
+  })
+  .catch(error => {
+    console.error(error);
+  });
